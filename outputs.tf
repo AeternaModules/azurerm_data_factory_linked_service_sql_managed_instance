163739dc55437c23db51dc40c0bc6f4664dc961e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,56 +1,51 @@
-output "data_factory_linked_service_sql_managed_instances" {
-  description = "All data_factory_linked_service_sql_managed_instance resources"
-  value       = azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances
-  sensitive   = true
-}
 output "data_factory_linked_service_sql_managed_instances_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.annotations }
 }
 output "data_factory_linked_service_sql_managed_instances_connection_string" {
-  description = "List of connection_string values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.connection_string]
+  description = "Map of connection_string values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.connection_string }
   sensitive   = true
 }
 output "data_factory_linked_service_sql_managed_instances_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.data_factory_id }
 }
 output "data_factory_linked_service_sql_managed_instances_description" {
-  description = "List of description values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.description]
+  description = "Map of description values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.description }
 }
 output "data_factory_linked_service_sql_managed_instances_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_sql_managed_instances_key_vault_connection_string" {
-  description = "List of key_vault_connection_string values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.key_vault_connection_string]
+  description = "Map of key_vault_connection_string values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.key_vault_connection_string }
 }
 output "data_factory_linked_service_sql_managed_instances_key_vault_password" {
-  description = "List of key_vault_password values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.key_vault_password]
+  description = "Map of key_vault_password values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.key_vault_password }
 }
 output "data_factory_linked_service_sql_managed_instances_name" {
-  description = "List of name values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.name]
+  description = "Map of name values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.name }
 }
 output "data_factory_linked_service_sql_managed_instances_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.parameters }
 }
 output "data_factory_linked_service_sql_managed_instances_service_principal_id" {
-  description = "List of service_principal_id values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.service_principal_id]
+  description = "Map of service_principal_id values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.service_principal_id }
 }
 output "data_factory_linked_service_sql_managed_instances_service_principal_key" {
-  description = "List of service_principal_key values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.service_principal_key]
+  description = "Map of service_principal_key values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.service_principal_key }
   sensitive   = true
 }
 output "data_factory_linked_service_sql_managed_instances_tenant" {
-  description = "List of tenant values across all data_factory_linked_service_sql_managed_instances"
-  value       = [for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : v.tenant]
+  description = "Map of tenant values across all data_factory_linked_service_sql_managed_instances, keyed the same as var.data_factory_linked_service_sql_managed_instances"
+  value       = { for k, v in azurerm_data_factory_linked_service_sql_managed_instance.data_factory_linked_service_sql_managed_instances : k => v.tenant }
 }
 
